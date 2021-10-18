32c17b9d61e56436c72491b7d670c44b54fb4366
GabrielJorge <gabrieljorge.ca@gmail.com>
2021-10-18 13:45:37 -0400

    Exercices 1 to 6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+import About from './components/About';
+import Users from './components/Users';
 
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/users">Users</Link>
+      <Route exact path="/" >
       <Home />
+      </Route>
+      <Route path="/about" component={About} />
+      <Route path="/users" component={Users} />
+      </BrowserRouter>
     );
   }
 }
