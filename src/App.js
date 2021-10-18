52f45693d582b3ac72fbb5f054fe99277b4555da
GabrielJorge <gabrieljorge.ca@gmail.com>
2021-10-18 14:42:35 -0400

    add exercise 9

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
       <Home />
       </Route>
       <Route path="/about" component={About} />
-      <Route path="/users" render={(props) => <Users {...props} greetingsMessage="Good Morning" />}/>
+      <Route path="/users/:id" render={(props) => <Users {...props} greetingsMessage="Good Morning" />}/>
       </BrowserRouter>
     );
   }
--- src/components/Users.js
+++ src/components/Users.js
@@ -3,11 +3,12 @@ import React, { Component } from 'react';
 
 class Users extends Component {
   render() {
+    const {id} = this.props.match.params;
     const { greetingsMessage } = this.props
     return (
       <div>
         <h2>Users</h2>
-        <p> { greetingsMessage }, My awesome Users component </p>
+        <p> { greetingsMessage } { id }, My awesome Users component </p>
       </div>
     );
   }
